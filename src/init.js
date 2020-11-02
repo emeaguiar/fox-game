b854b9a77d0d4afe0b4c60b580a0d8182c8a64d4
Mario Aguiar <me@marioaguiar.net>
2020-11-02 14:14:55 -0600

    Hook up buttons

--- src/init.js
+++ src/init.js
@@ -1,9 +1,10 @@
 import game from './gameState';
-
-const TICK_RATE = 3000;
+import { TICK_RATE } from './constants';
+import initButtons from './buttons';
 
 const init = async () => {
   console.log( "Starting a game" );
+  initButtons( game.handleUserAction );
 
   let nextTimeToTick = Date.now();
 
